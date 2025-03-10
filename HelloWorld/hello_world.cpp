54783217701fa76566d823ae72da7f9fcbe72cf0
Vahe Yeghiazaryan <yeghiazarian.vahe@gmail.com>
2025-03-11 00:08:19 +0400

    Hello, World! This is first steps into C++.

---
+++ HelloWorld/hello_world.cpp
@@ -0,0 +1,13 @@
+#include <iostream>  // Include the standard I/O stream library
+
+int main() {  
+    // The main function is the entry point of a C++ program
+
+    std::cout << "Hello, World!" << std::endl;
+    // std::cout is used to print output to the console.
+    // "Hello, World!" is a string literal stored in the read-only data segment.
+    // std::endl moves the cursor to the next line (similar to '\n').
+
+    return 0;  
+    // Returning 0 indicates that the program executed successfully.
+}
